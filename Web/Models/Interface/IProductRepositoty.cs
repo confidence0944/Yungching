19db874807e682703a917d64b73d1e255c7416fb
leonHuang1123 <leon_huang@engsound.com.tw>
2022-04-19 15:55:48 +0800

    新增ProductRepository

---
+++ Web/Models/Interface/IProductRepositoty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.DBEntities;
+
+namespace Web.Models.Interface
+{
+    public interface IProductRepositoty
+    {
+        Product Get(int productID);
+
+        void Create(Product instance);
+
+        void Update(Product instance);
+
+        void Delete(Product instance);
+
+        IQueryable<Product> GetAll();
+
+        void SaveChanges();
+    }
+}
---
+++ Web/Models/Repository/ProductRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Web.DBEntities;
+using Web.Models.Interface;
+
+namespace Web.Models.Repository
+{
+    public class ProductRepository : IProductRepositoty, IDisposable
+    {
+        private NorthwindContext db;
+
+        public ProductRepository()
+        {
+            db = new NorthwindContext();
+        }
+
+        public Product Get(int productID)
+        {
+            return db.Products.FirstOrDefault(x => x.ProductId == productID);
+        }
+
+        public IQueryable<Product> GetAll()
+        {
+            return db.Products.OrderBy(x => x.ProductId);
+        }
+
+        public void Create(Product instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                db.Products.Add(instance);
+                SaveChanges();
+            }
+        }
+
+        public void Delete(Product instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                db.Entry(instance).State = EntityState.Deleted;
+                SaveChanges();
+            }
+        }
+
+        public void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(Product instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                db.Entry(instance).State = EntityState.Modified;
+                SaveChanges();
+            }
+        }
+
+
+        public void Dispose()
+        {
+            if (db != null)
+            {
+                db.Dispose();
+                db = null;
+            }
+        }
+    }
+}
