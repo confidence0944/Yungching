6b269127d657dbe2ed7ed4a2d30051ccf0a93451
leonHuang1123 <leon_huang@engsound.com.tw>
2022-04-19 16:04:25 +0800

    新增Product列表

--- Web/Controllers/ProductController.cs
+++ Web/Controllers/ProductController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web.Models.Interface;
+using Web.Models.Repository;
 
 namespace Web.Controllers
 {
     public class ProductController : Controller
     {
+        private IProductRepositoty productRepositoty;
+
+        public ProductController()
+        {
+            productRepositoty = new ProductRepository();
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var data = productRepositoty.GetAll().ToList();
+            return View(data);
         }
     }
 }
